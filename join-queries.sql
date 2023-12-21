46e922576926d8d94b458f910359f939693b62d1
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-12-21 18:06:58 +0100

    add: join queries nr. 4, 5

--- join-queries.sql
+++ join-queries.sql
@@ -23,10 +23,22 @@ ON `course_teacher`.`teacher_id` = `teachers`.`id`
 WHERE `teachers`.`name` LIKE 'fulvio' AND `teachers`.`surname` LIKE 'amato';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT CONCAT(`students`.`surname`, ' ', `students`.`name`) AS `student`, CONCAT(`courses`.`name`, ' ', `courses`.`cfu`) AS `course`, CONCAT(`degrees`.`name`, ' ', `degrees`.`level`) AS `degree`
+FROM `students`
+JOIN `degrees` 
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `courses`
+ON `courses`.`degree_id`= `degrees`.`id`
+ORDER BY `student` ASC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT CONCAT(`degrees`.`name`, ' ', `degrees`.`level`) AS `degree`, CONCAT(`courses`.`name`, ' ', `courses`.`cfu`) AS `course`, CONCAT(`teachers`.`name`, ' ', `teachers`.`surname`) AS `teacher`
+FROM `degrees`
+JOIN `courses`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher` 
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
