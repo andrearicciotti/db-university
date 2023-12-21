6ead99b83d994fb5a567c4a5ab262bc4194f69d7
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-12-21 17:47:18 +0100

    add: join queries nr. 1, 2, 3

--- join-queries.sql
+++ join-queries.sql
@@ -1,11 +1,26 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`, `degrees`.`level`
+FROM `students`
+JOIN `degrees` 
+ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` LIKE '%biologia%'
+ORDER BY `students`.`name`;
 
 -- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
-
+SELECT `degrees`.`name`, `degrees`.`level`, `departments`.`name`
+FROM `degrees`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` LIKE '%neuroscienz%';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+SELECT `courses`.`name` AS `course_name`, CONCAT(`teachers`.`name`, ' ', `teachers`.`surname`) AS `teacher`
+FROM `courses`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `teachers`.`name` LIKE 'fulvio' AND `teachers`.`surname` LIKE 'amato';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
